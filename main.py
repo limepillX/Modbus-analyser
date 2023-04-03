d240d85caa23b832b95bbc1462a9fcc90b8538af
tamu <justtedyt@gmail.com>
2023-04-03 14:22:04 +0400

    Getting raw data, server semi-working

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import dataclasses
+
 from pyModbusTCP.client import ModbusClient
 from time import sleep
 from datetime import datetime
@@ -7,16 +9,29 @@ HOST = os.getenv('IP')
 PORT = os.getenv('PORT')
 
 
-def get_parameter(x):
-    return 256 + (x * 10)
+class Parameter:
+    def __init__(self, number):
+        self.number = number
+        self.id = None
+        self.hiword = None
+        self.loword = None
+
+    def get_id(self, client):
+        self.id = hex(client.read_holding_registers(256 + (self.number * 10))[0])
+        return self.id
+
+    def get_hiword(self, client):
+        self.hiword = client.read_holding_registers(256 + (self.number * 10) + 6)[0]
+        return self.hiword
+
+    def get_loword(self, client):
+        self.loword = client.read_holding_registers(256 + (self.number * 10) + 7)[0]
+        return self.loword
 
 
 device_address = ['Адрес устройства на шине MODBUS', 11]
 serial_number_controller = ['Серийный номер модуля управления', 12]
 software_version = ['Версия встроенного программного обеспечения', 13]
-tal_block = ['ПОЛОЖЕНИЕ ТАЛЬ-БЛОКА', get_parameter(6)]
-hook_load = ['НАГРУЗКА НА КРЮК', get_parameter(7)]
-pressure_pj_input = ['МОМЕНТ НА РОТОРЕ', get_parameter(18)]
 
 
 def logger(func):
@@ -26,7 +41,7 @@ def logger(func):
             func()
         except BaseException as ex:
             send_log(f'{func.__name__}\terr->\t{type(ex).__name__}')
-            exit('Shutting down...')
+            raise ex
         else:
             send_log(f'Функция {func.__name__} завершила работу без ошибок')
 
@@ -45,22 +60,36 @@ def send_log(text: str = ''):
 @logger
 def start_client():
     client = ModbusClient(host=HOST, port=int(PORT), unit_id=206)
-    state = None
+    hiword = None
+    loword = None
     try:
         send_log('Клиент запускается...')
         client.open()
         send_log('Клиент запущен')
 
-        to_get = hook_load
+        # to_get = tal_block
+        # curr = Parameter(11)
+        # while True:
+        #     curr.get_id(client)
+        #     curr.get_hiword(client)
+        #     curr.get_loword(client)
+        #     if hiword != curr.hiword or loword != curr.loword:
+        #         hiword = curr.hiword
+        #         loword = curr.loword
+        #         send_log(f'{curr.id} | {curr.loword} | {curr.loword}')
+        #
+        #     sleep(5)
 
-        while True:
-            if state != client.read_holding_registers(to_get[1]):
-                state = client.read_holding_registers(to_get[1])
-                send_log(f'Запрос: {to_get}')
-                send_log(f'Ответ: {to_get[0]} = {",".join([str(i) for i in state])}')
-
-            sleep(5)
-
+        n = 0
+        for i in range(40):
+            curr = Parameter(i)
+            curr.get_id(client)
+            curr.get_hiword(client)
+            curr.get_loword(client)
+            if curr.hiword != 65535:
+                print(f'{curr.number} - {curr.id} - {curr.hiword} - {curr.loword}')
+            else:
+                print(f'{curr.number} - не работает')
     finally:
         client.close()
 
